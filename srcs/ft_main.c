88abf64b691e9c4b8001ff0fda013f577651dcf7
Zheylkoss <100791060+Zheylkoss@users.noreply.github.com>
2022-10-27 18:25:23 +0200

    new

--- srcs/ft_main.c
+++ srcs/ft_main.c
@@ -3,22 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_main.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zhamdouc <zhamdouc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zakariyahamdouchi <zakariyahamdouchi@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 19:02:02 by zakariyaham       #+#    #+#             */
-/*   Updated: 2022/10/26 19:10:35 by zhamdouc         ###   ########.fr       */
+/*   Updated: 2022/10/27 18:25:15 by zakariyaham      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	deal_key(int key, void *param)
+{
+	ft_putchar("X");
+	return (0);
+}
+
 int	main(void)
 {
 	void	*mlx;
-	void	*mlx_win;
+	void	*win_ptr;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	
+	win_ptr = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_key_hook(win_ptr, deal_key, 0);
 	mlx_loop(mlx);
 }
