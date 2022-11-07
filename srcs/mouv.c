81772ee7947bb0c862e81e860a791baee88767e2
Zakariya HAMDOUCHI <zhamdouc@e3r8p20.clusters.42paris.fr>
2022-11-07 17:55:30 +0100

    new

--- srcs/mouv.c
+++ srcs/mouv.c
@@ -6,7 +6,7 @@
 /*   By: zhamdouc <zhamdouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 15:12:17 by zhamdouc          #+#    #+#             */
-/*   Updated: 2022/11/07 16:27:05 by zhamdouc         ###   ########.fr       */
+/*   Updated: 2022/11/07 17:50:20 by zhamdouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,14 @@ int	check_mouv_d(t_list *list)
 		list->ec_copy = list->ec_copy - 1;
 		return (0);
 	}
-	if (list->tab[i][j + 1] == 'E')
+	if (list->tab[i][j + 1] == 'E' && list->ec_copy != 1)
 	{
-		list->tab[i][j + 1] = 'P';
-		list->tab[i][j] = '0';
-		list->ec_copy = list->ec_copy - 1;
-		return (0);
+		list->tab[i][j + 1] = 'E';
+		list->tab[i][j] = 'P';
+		return (1);
 	}
+	else if (list->tab[i][j + 1] == 'E' && list->ec_copy == 1)
+		list->ec_copy = list->ec_copy - 1;
 	return (1);
 }
 
@@ -77,13 +78,14 @@ int	check_mouv_a(t_list *list)
 		list->ec_copy = list->ec_copy - 1;
 		return (0);
 	}
-	if (list->tab[i][j - 1] == 'E')
+	if (list->tab[i][j - 1] == 'E' && list->ec_copy != 1)
 	{
-		list->tab[i][j - 1] = 'P';
-		list->tab[i][j] = '0';
-		list->ec_copy = list->ec_copy - 1;
-		return (0);
+		list->tab[i][j - 1] = 'E';
+		list->tab[i][j] = 'P';
+		return (1);
 	}
+	else if (list->tab[i][j - 1] == 'E' && list->ec_copy == 1)
+		list->ec_copy = list->ec_copy - 1;
 	return (1);
 }
 
@@ -113,13 +115,14 @@ int	check_mouv_w(t_list *list)
 		list->ec_copy = list->ec_copy - 1;
 		return (0);
 	}
-	if (list->tab[i - 1][j] == 'E')
+	if (list->tab[i - 1][j] == 'E' && list->ec_copy != 1)
 	{
-		list->tab[i - 1][j] = 'P';
-		list->tab[i][j] = '0';
-		list->ec_copy = list->ec_copy - 1;
-		return (0);
+		list->tab[i - 1][j] = 'E';
+		list->tab[i][j] = 'P';
+		return (1);
 	}
+	else if (list->tab[i - 1][j] == 'E' && list->ec_copy == 1)
+		list->ec_copy = list->ec_copy - 1;
 	return (1);
 }
 
@@ -149,12 +152,13 @@ int	check_mouv_s(t_list *list)
 		list->ec_copy = list->ec_copy - 1;
 		return (0);
 	}
-	if (list->tab[i + 1][j] == 'E')
+	if (list->tab[i + 1][j] == 'E' && list->ec_copy != 1)
 	{
-		list->tab[i + 1][j] = 'P';
-		list->tab[i][j] = '0';
-		list->ec_copy = list->ec_copy - 1;
-		return (0);
+		list->tab[i + 1][j] = 'E';
+		list->tab[i][j] = 'p';
+		return (1);
 	}
+	else if (list->tab[i + 1][j] == 'E' && list->ec_copy == 1)
+		list->ec_copy = list->ec_copy - 1;
 	return (1);
 }
